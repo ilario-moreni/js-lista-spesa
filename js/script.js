2601c6d51f2ae479a2293ad458940f8519924fd2
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-03 12:53:14 +0100

    task done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('caccapuzza')
+/* 1 */
+
+let shopList = ['Yogurt', 'Banane', 'Pasta', 'Passata', 'Latte', 'Caffè', 'Carta igenica', 'Riso'];
+
+/* indice shopList */
+let i = 0;
+
+let listContent = '';
+
+while(i<shopList.length){
+    listContent += `<li class="list_item">${shopList[i]}</li>`;
+    i++;
+}
+
+const showList = document.getElementById('my_shop_list');
+
+showList.innerHTML += listContent;
+
+console.log(listContent);
