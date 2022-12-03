5c2e705923f510d28ac5bf67fe195b7546ed6704
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-03 12:58:26 +0100

    code validate and commented

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@
 
 let shopList = ['Yogurt', 'Banane', 'Pasta', 'Passata', 'Latte', 'Caffè', 'Carta igenica', 'Riso'];
 
-/* indice shopList */
+/* shopList index */
 let i = 0;
 
 let listContent = '';
 
+
+/* 2 */
+/* while index < shopList length insert to listContent an li containing the shopList[i] */
 while(i<shopList.length){
     listContent += `<li class="list_item">${shopList[i]}</li>`;
     i++;
 }
 
+
+/* important to give const */
 const showList = document.getElementById('my_shop_list');
 
+
+/* important to add listContent to the innerHTML */
 showList.innerHTML += listContent;
 
 console.log(listContent);
